a896459d0efb6e235c35bfbb1544119d2d3d165f
muhhae <haekal.muhamat@gmail.com>
2024-10-12 17:54:59 +0700

    fix

--- embed-counter.ino
+++ embed-counter.ino
@@ -1,14 +1,15 @@
-const int segments[7] = {14, 27, 33, 25, 26, 12, 13};
+const int segments[7] = {32, 33, 21, 19, 18, 26, 25};
 
 void setup() {
+    Serial.begin(9600);
     for (int i = 0; i < 7; i++) {
         pinMode(segments[i], OUTPUT);
     }
-    pinMode(22, INPUT)
-    pinMode(23, INPUT)
+    pinMode(22, INPUT);
+    pinMode(23, INPUT);
 }
 
-const int pattern[10][7] = {
+const int patterns[10][7] = {
     {0, 0, 0, 0, 0, 0, 1}, // 0
     {1, 0, 0, 1, 1, 1, 1}, // 1
     {0, 0, 1, 0, 0, 1, 0}, // 2
@@ -23,24 +24,30 @@ const int pattern[10][7] = {
 
 void showNumber(int n) {
     for (int i = 0; i < 7; i++) {
-        digitalWrite(segments[i], pattern[n][i]);
+        digitalWrite(segments[i], patterns[n][i]);
     }
 }
 
 int n = 0;
+bool changed = false;
 
 void loop() {
-    if (!digitalRead(22)) {
+    changed = false;
+    Serial.printf("%d %d\n", digitalRead(22), digitalRead(23));
+    if (digitalRead(22) == LOW) {
         n--;
-    }
-    if (!digitalRead(23)) {
+        changed = true;
+    } else if (digitalRead(23) == LOW) {
         n++;
+        changed = true;
     }
+
     if (n > 9) {
         n = 0;
+    } else if (n < 0) {
+        n = 9;
     }
-    if (n < 0) {
-        n = 9
-    }
+
     showNumber(n);
+    if (changed) delay(200);
 }
